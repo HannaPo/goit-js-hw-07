3422e63403e9f2701522c1886a56e6aafb76fd45
HannaPo <114150796+HannaPo@users.noreply.github.com>
2023-03-04 16:40:48 +0200

    Task_1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const createGalleryCard = ({ preview, original, description }) => `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+
+const markup = galleryItems.map(el => createGalleryCard(el)).join('');
+
+galleryEl.insertAdjacentHTML('afterbegin', markup);
+
+galleryEl.addEventListener('click', handleClickImg);
+
+function handleClickImg(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const bannerUrl = e.target.dataset.source;
+
+  const modal = basicLightbox.create(`<img class="gallery__image" src="${bannerUrl}">`, {
+    onShow: () => {
+      window.addEventListener('keydown', handleKeydownEsc);
+    },
+    onClose: () => {
+      window.removeEventListener('keydown', handleKeydownEsc);
+    },
+  });
+  modal.show();
+
+  function handleKeydownEsc(e) {
+    if (e.code !== 'Escape') return;
+    modal.close();
+  }
+}
